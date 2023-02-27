155ce479a608c1de1f156d8b43709205193ea34f
Jonathan <jonathan.davis@branch.technology>
2023-02-26 19:02:58 -0500

    added insertion sort

--- src/sorting.rs
+++ src/sorting.rs
@@ -1,7 +1,8 @@
 fn selection(unsorted: &mut Vec<i32>) {
-    for x in 0..unsorted.len() {
+    let length = unsorted.len();
+    for x in 0..length {
         let mut min_index = x;
-        for y in x..unsorted.len() {
+        for y in x..length {
             if unsorted[y] < unsorted[min_index] {
                 min_index = y
             }
@@ -10,10 +11,63 @@ fn selection(unsorted: &mut Vec<i32>) {
     }
 }
 
+fn insertion(unsorted: &mut Vec<i32>) {
+    let length = unsorted.len();
+    for x in 1..length {
+        let mut i = x;
+        while i > 0 && unsorted[i] < unsorted[i - 1] {
+            unsorted.swap(i, i - 1);
+            i = i - 1;
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use rand::Rng;
-    use crate::sorting::selection;
+
+    use crate::sorting::{insertion, selection};
+
+    #[test]
+    fn test_insertion_dont_move() {
+        let immut_vec = vec![0, 1, 2, 3, 5, 6];
+        let mut test_vec = vec![0, 1, 2, 3, 5, 6];
+        insertion(&mut test_vec);
+        assert_eq!(immut_vec, test_vec)
+    }
+
+    #[test]
+    fn test_insertion() {
+        let immut_vec = vec![0, 1, 2, 3, 5, 6];
+        let mut test_vec = vec![0, 2, 1, 3, 5, 6];
+        insertion(&mut test_vec);
+        assert_eq!(immut_vec, test_vec)
+    }
+
+    #[test]
+    fn test_insertion_dup() {
+        let immut_vec = vec![0, 2, 2, 3, 5, 6];
+        let mut test_vec = vec![0, 2, 2, 3, 5, 6];
+        insertion(&mut test_vec);
+        assert_eq!(immut_vec, test_vec)
+    }
+
+    #[test]
+    fn test_large_insertion_against_builtin() {
+        let mut rng = rand::thread_rng();
+        let length = 0..10000;
+        let mut rand_vec: Vec<i32> = (length.clone()).map(|_| rng.gen_range(length.clone())).collect();
+        let mut test_vec = rand_vec.clone();
+        let builtin_start = chrono::Local::now();
+        rand_vec.sort();
+        let builtin_done = chrono::Local::now();
+        let impl_start = chrono::Local::now();
+        insertion(&mut test_vec);
+        let impl_done = chrono::Local::now();
+        println!("builtin took {}", builtin_done - builtin_start);
+        println!("implementation took {}", impl_done - impl_start);
+        assert_eq!(rand_vec, test_vec)
+    }
 
     #[test]
     fn test_selection_dont_move() {
@@ -44,14 +98,15 @@ mod tests {
         let mut rng = rand::thread_rng();
         let length = 0..10000;
         let mut rand_vec: Vec<i32> = (length.clone()).map(|_| rng.gen_range(length.clone())).collect();
-
         let mut test_vec = rand_vec.clone();
-        println!("builtin start: {}", chrono::Local::now().format("%H:%M:%S.%f"));
+        let builtin_start = chrono::Local::now();
         rand_vec.sort();
-        println!("builtin done: {}", chrono::Local::now().format("%H:%M:%S.%f"));
-        println!("implementation start: {}", chrono::Local::now().format("%H:%M:%S.%f"));
+        let builtin_done = chrono::Local::now();
+        let impl_start = chrono::Local::now();
         selection(&mut test_vec);
-        println!("implementation done: {}", chrono::Local::now().format("%H:%M:%S.%f"));
+        let impl_done = chrono::Local::now();
+        println!("builtin took {}", builtin_done - builtin_start);
+        println!("implementation took {}", impl_done - impl_start);
         assert_eq!(rand_vec, test_vec)
     }
 }
