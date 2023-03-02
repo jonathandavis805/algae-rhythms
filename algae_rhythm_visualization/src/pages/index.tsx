19a78fd05cd3f590901900de9a85040b4dad3376
Jonathan <jonathan.davis@branch.technology>
2023-03-01 21:33:37 -0500

    add slider and state update

--- algae_rhythm_visualization/src/pages/index.tsx
+++ algae_rhythm_visualization/src/pages/index.tsx
@@ -1,10 +1,12 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import {Inter} from 'next/font/google'
 import styles from '@/styles/Home.module.css'
-import React from "react";
+import React, {useEffect, useState} from "react";
 import {Scatter} from "react-chartjs-2";
 import {Chart, Legend, LinearScale, LineElement, PointElement, Tooltip} from "chart.js";
+import Slider from 'rc-slider';
+import 'rc-slider/assets/index.css';
+
 
 const inter = Inter({subsets: ['latin']})
 
@@ -21,18 +23,31 @@ export const options = {
 export const data = {
     datasets: [
         {
-            label: 'A dataset',
-            data: Array.from({ length: 100 }, (el,e) => ({
+            label: 'selection',
+            data: Array.from({length: 100}, (el, e) => ({
                 x: e,
                 y: Math.random()
             })),
             backgroundColor: 'rgba(255, 99, 132, 1)',
+        },
+        {
+            label: 'insertion',
+            data: Array.from({length: 100}, (el, e) => ({
+                x: e,
+                y: Math.random()
+            })),
+            backgroundColor: 'rgb(132,189,255)',
         },
     ],
 };
 
 
 export default function Home() {
+    const [index, setIndex] = useState(0);
+    useEffect(() => {
+        console.log("index changed")
+
+    }, [index])
     return (
         <>
             <Head>
@@ -42,91 +57,12 @@ export default function Home() {
                 <link rel="icon" href="/favicon.ico"/>
             </Head>
             <main className={styles.main}>
-                <div className={styles.description}>
-                    <p>
-                        Get started by editing&nbsp;
-                        <code className={styles.code}>src/pages/index.tsx</code>
-                    </p>
-                    <div>
-                        <a
-                            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-                            target="_blank"
-                            rel="noopener noreferrer"
-                        >
-                            By{' '}
-                            <Image
-                                src="/vercel.svg"
-                                alt="Vercel Logo"
-                                className={styles.vercelLogo}
-                                width={100}
-                                height={24}
-                                priority
-                            />
-                        </a>
-                    </div>
-                </div>
-                <div>
-                    <Scatter options={options} data={data} />;
-                    <Scatter options={options} data={data} />;
-                </div>
-                <div className={styles.grid}>
-                    <a
-                        href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-                        className={styles.card}
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        <h2 className={inter.className}>
-                            Docs <span>-&gt;</span>
-                        </h2>
-                        <p className={inter.className}>
-                            Find in-depth information about Next.js features and&nbsp;API.
-                        </p>
-                    </a>
-
-                    <a
-                        href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-                        className={styles.card}
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        <h2 className={inter.className}>
-                            Learn <span>-&gt;</span>
-                        </h2>
-                        <p className={inter.className}>
-                            Learn about Next.js in an interactive course with&nbsp;quizzes!
-                        </p>
-                    </a>
-
-                    <a
-                        href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-                        className={styles.card}
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        <h2 className={inter.className}>
-                            Templates <span>-&gt;</span>
-                        </h2>
-                        <p className={inter.className}>
-                            Discover and deploy boilerplate example Next.js&nbsp;projects.
-                        </p>
-                    </a>
-
-                    <a
-                        href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-                        className={styles.card}
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        <h2 className={inter.className}>
-                            Deploy <span>-&gt;</span>
-                        </h2>
-                        <p className={inter.className}>
-                            Instantly deploy your Next.js site to a shareable URL
-                            with&nbsp;Vercel.
-                        </p>
-                    </a>
-                </div>
+                <Scatter options={options} data={data}/>
+                <Slider onChange={(e) => {
+                    if (typeof (e) == "number") {
+                        setIndex(e)
+                    }
+                }}/>
             </main>
         </>
     )
