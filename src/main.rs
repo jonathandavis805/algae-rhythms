afbaf8c56ab922434bf7fd47d577517082ced4d7
Jonathan <jonathan.davis@branch.technology>
2023-03-19 19:15:37 -0400

    state is updating

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
+use std::borrow::BorrowMut;
 use std::cmp::min;
 use std::fmt::format;
 use std::fs::File;
 use std::io::Read;
-use std::ops::Index;
+use std::ops::{Deref, Index};
 use std::slice::SliceIndex;
+use std::sync::{Arc, Mutex};
 
 use actix_web::{App, get, HttpResponse, HttpServer, post, Responder, web};
 use actix_web::web::Data;
@@ -14,17 +16,17 @@ use crate::sorting::{insertion, selection};
 mod sorting;
 
 #[post("save")]
-async fn save_sorts() -> impl Responder {
-    print!("saving new sorts");
+async fn save_sorts(mut state: Data<State>) -> impl Responder {
     let mut rng = rand::thread_rng();
     let selection_file: File = File::create("./sorted/selection.json").unwrap();
     let insertion_file: File = File::create("./sorted/insertion.json").unwrap();
     let length = 0..1000;
     let mut rand_vec: Vec<i32> = (length.clone()).map(|_| rng.gen_range(length.clone())).collect();
     let mut selection_vec = rand_vec.clone();
-    selection(&mut selection_vec, Some(selection_file));
+    selection(&mut selection_vec, Option::Some(selection_file));
     let mut insertion_vec = rand_vec.clone();
-    insertion(&mut insertion_vec, Some(insertion_file));
+    insertion(&mut insertion_vec, Option::Some(insertion_file));
+    state.update_state();
     HttpResponse::Ok().append_header(("Access-Control-Allow-Origin", "*")).body(format!("new sorts saved"))
 }
 
@@ -38,11 +40,13 @@ async fn get_sort(params: web::Path<(String, usize)>, state: Data<State>) -> imp
     let index = params_inner.1;
     match params_inner.0.as_str() {
         "selection" => {
-            let val = Some(state.selection[index].clone());
+            let selection = state.selection.lock().unwrap();
+            let val = Some(selection[index].clone());
             HttpResponse::Ok().append_header(("Access-Control-Allow-Origin", "*")).body(format!("{:?}", val))
         }
         "insertion" => {
-            let val = Some(state.insertion[index].clone());
+            let insertion = state.insertion.lock().unwrap();
+            let val = Some(insertion[index].clone());
             HttpResponse::Ok().append_header(("Access-Control-Allow-Origin", "*")).body(format!("{:?}", val))
         }
         _ => {
@@ -53,25 +57,34 @@ async fn get_sort(params: web::Path<(String, usize)>, state: Data<State>) -> imp
 
 #[derive(Clone)]
 struct State {
-    selection: Vec<Vec<i32>>,
-    insertion: Vec<Vec<i32>>,
+    selection: Arc<Mutex<Vec<Vec<i32>>>>,
+    insertion: Arc<Mutex<Vec<Vec<i32>>>>,
+}
+
+impl State {
+    fn update_state(&self) {
+        let mut selection_data = Vec::new();
+        File::open("/Users/jd/repos/algae-rhythms/sorted/selection.json").unwrap().read_to_end(&mut selection_data).unwrap();
+        let mut insertion_data = Vec::new();
+        File::open("/Users/jd/repos/algae-rhythms/sorted/insertion.json").unwrap().read_to_end(&mut insertion_data).unwrap();
+        let mut ins = self.insertion.lock().unwrap();
+        let mut ins_vec: Vec<Vec<i32>> = serde_json::from_slice(insertion_data.as_ref()).expect("TODO: panic message");
+        *ins = ins_vec;
+        let mut sel = self.selection.lock().unwrap();
+        let mut sel_vec: Vec<Vec<i32>> = serde_json::from_slice(selection_data.as_ref()).expect("TODO: panic message");
+        *sel = sel_vec;
+    }
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        let mut selection_data = Vec::new();
-        File::open("/Users/jd/repos/algae-rhythms/sorted/selection.json").unwrap().read_to_end(&mut selection_data).unwrap();
-        let selection_vec: Vec<Vec<i32>> = serde_json::from_slice(selection_data.as_ref()).expect("TODO: panic message");
-        let mut insertion_data = Vec::new();
-        File::open("/Users/jd/repos/algae-rhythms/sorted/insertion.json").unwrap().read_to_end(&mut insertion_data).unwrap();
-        let insertion_vec: Vec<Vec<i32>> = serde_json::from_slice(insertion_data.as_ref()).expect("TODO: panic message");
-        let state = State {
-            selection: selection_vec,
-            insertion: insertion_vec,
-        };
+    let state = State {
+        selection: Arc::new(Mutex::new(Vec::new())),
+        insertion: Arc::new(Mutex::new(Vec::new())),
+    };
+    HttpServer::new(move || {
         App::new()
-            .app_data(Data::new(state))
+            .app_data(Data::new(state.clone()))
             .service(get_sort)
             .service(save_sorts)
     })
